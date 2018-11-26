3467f1b88ab9059b11831e4badd3e7d16a12ea41
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-11-26 17:12:25 +0000

    added buttons component
    Co-Authored-By: ray <smith.raylene@gmail.com>
    Co-Authored-By: shannon <shannongamby@hotmail.com>
    Co-Authored-By: alfie <runticle@gmail.com>
    Co-Authored-By: todd <toddpla@gmail.com>

---
+++ src/components/buttons.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class Buttons extends React.Component {
+
+  render() {
+
+    return (
+              <div id="buttons">
+                <div className="button">✏️</div>
+                <div className="button">❌</div>
+                <div className="button">⭐️</div>
+                <div className="button">💬</div>
+                <div className="button">➕</div>
+              </div>
+          )
+    }
+}
+
+export default Buttons;
