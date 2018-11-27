985d4d6cc6e163e0ff60e7dd70c02385b038ec84
Alfie Freeman <runticle@gmail.com>
2018-11-27 11:30:06 +0000

    adds email and pw state to login, adds event handler on change
    Co-Authored-By: Imogen Misso <imogenmisso@users.noreply.github.com>
    Co-Authored-By: Shannon Gamby <shannongamby@users.noreply.github.com>
    Co-Authored-By: Todd Langford-Archer <toddpla@gmail.com>
    Co-Authored-By: ray <smith.raylene@gmail.com>

--- src/components/login.js
+++ src/components/login.js
@@ -1,15 +1,47 @@
 import React from 'react';
 
 class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
   render () {
+
     return (
       <div id="login">
-        <input type="email" placeholder="Email"/>
-        <input type="password" placeholder="Password"/>
-        <button id="submit_login">Submit</button>
+        <form onSubmit={this.handleSubmit}>
+          <input type="email" id="email" placeholder="Email" onChange={this.handleChange}/>
+          <input type="password" id="password" placeholder="Password" onChange={this.handleChange}/>
+          <button id="submit_login">Submit</button>
+        </form>
       </div>
     )
   }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    console.log(event.target.id)
+    console.log(event.target.value)
+     this.setState({
+       [event.target.id]: event.target.value
+     });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    //fetch call to rails api??
+    //check details n shit ?
+  }
+
 }
 
 export default Login;
