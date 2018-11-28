4d805de3765d7f98d020a51f782bf6395e38d090
shannongamby <shannongamby@hotmail.com>
2018-11-28 11:23:54 +0000

    server works
    Co-Authored-By: Ray <smith.raylene@gmail.com>

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
-const PORT = '3000'
-const HOST = '127.0.0.1'
+const PORT = '8080'
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.set("view engine", "html")
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/', (req, res) => {
   res.render("index")
